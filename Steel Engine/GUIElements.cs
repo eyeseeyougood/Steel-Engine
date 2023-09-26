b08a158c2bbe0a5f7d0888ad73893bdc06e9fc9a
eyeseeyougood <97800461+eyeseeyougood@users.noreply.github.com>
2023-09-26 19:54:20 +0100

    Restructure
    + New structure for 'GUIElement' class

--- Steel Engine/GUIElements.cs
+++ Steel Engine/GUIElements.cs
@@ -10,46 +10,78 @@ using System.Drawing.Imaging;
 using System.Drawing;
 using static System.Formats.Asn1.AsnWriter;
 using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Steel_Engine.GUI
 {
-    public abstract class GUIElement
+    public class GUIElement
     {
-        public abstract void Render();
-        public abstract void CleanUp();
+        public Vector2 position; // arbitrary units
+        public Vector3 addedPosition;
+        public Vector2 anchor;
+        public int textureId { get; private set; }
+        public Bitmap texture { get; private set; }
+        public GameObject renderObject { get; private set; }
+        public List<string> textures { get; private set; }
+
+        public GUIElement()
+        {
+            textures = new List<string>();
+
+            renderObject = new GameObject(RenderShader.ShadeTextureUnit, RenderShader.ShadeTextureUnit);
+        }
+
+        public void ApplyTexture(Bitmap bmp)
+        {
+            texture = bmp;
+        }
+
+        public virtual void Render()
+        {
+            Vector3 camRight = InfoManager.engineCamera.Right;
+            Vector3 camUp = InfoManager.engineCamera.Up;
+            Vector2 remappedAnchor = Vector2.Zero;
+            remappedAnchor = new Vector2(anchor.X * 0.933f, -anchor.Y * 0.522f * (InfoManager.windowSize.X / InfoManager.windowSize.Y));
+            float hh = MathF.Tan(MathHelper.DegreesToRadians(InfoManager.engineCamera.Fov) / 2.0f) * 1;
+            float aspect = InfoManager.windowSize.X / InfoManager.windowSize.Y;
+            renderObject.position = InfoManager.engineCamera.Position + InfoManager.engineCamera.Front * 1
+                + remappedAnchor.X * camRight * hh * aspect
+                + remappedAnchor.Y * camUp * hh
+                + camRight * addedPosition.X * 0.007f
+                + camUp * addedPosition.Y * 0.007f;
+            renderObject.SetRotation(InfoManager.engineCamera.GetViewMatrix().ExtractRotation().Inverted());
+            renderObject.Render();
+        }
+        public virtual void CleanUp()
+        {
+            foreach (string _texture in textures)
+            {
+                File.Delete(InfoManager.currentDir + @$"\Temp\{_texture}.png");
+            }
+        }
     }
 
     public class GUIText : GUIElement
     {
-        public Vector2 position; // screen space position
-        public Vector3 addedPosition;
         public string text;
         public string font;
         public float size;
         public float scale;
-        public Vector2 anchor;
-        private int textureId;
-        private Bitmap texture;
-        public GameObject renderObject { get; private set; }
         private Rectangle rect;
-        private List<string> textures = new List<string>();
-
-        public GUIText(Vector3 position, Vector2 anchor, float scale, string text, string font, float size)
+        public GUIText(Vector3 position, Vector2 anchor, float scale, string text, string font, float size) : base()
         {
-            texture = GUIManager.Write_Text(text, font, size);
+            ApplyTexture(GUIManager.Write_Text(text, font, size));
 
             this.text = text;
             this.font = font;
             this.size = size;
             this.anchor = anchor;
-            this.addedPosition = position;
+            addedPosition = position;
 
             if (!textures.Contains(text))
                 textures.Add(text);
 
             texture.Save(InfoManager.currentDir + @$"\Temp\{text}.png");
-
-            renderObject = new GameObject(RenderShader.ShadeTextureUnit, RenderShader.ShadeTextureUnit);
 
             this.scale = scale;
 
@@ -71,13 +103,13 @@ namespace Steel_Engine.GUI
 
             if (!File.Exists(InfoManager.currentDir + @$"\Temp\{text}.png"))
             {
-                texture = GUIManager.Write_Text(text, font, size);
+                ApplyTexture(GUIManager.Write_Text(text, font, size));
                 Rectangle dst = new Rectangle();
                 dst.X = (int)((float)(rect.Width - texture.Width) / 2.0f);
                 dst.Y = (int)((float)(rect.Height - texture.Height) / 2.0f);
                 dst.Width = texture.Width;
                 dst.Height = texture.Height;
-                texture = GUIManager.Write_Text(text, font, size, dst, rect);
+                ApplyTexture(GUIManager.Write_Text(text, font, size, dst, rect));
                 texture.Save(InfoManager.currentDir + @$"\Temp\{text}.png");
             }
 
@@ -85,30 +117,10 @@ namespace Steel_Engine.GUI
             renderObject.mesh = OBJImporter.LoadOBJFromPath(InfoManager.currentDir + @$"\EngineResources\EngineModels\Quad.obj", true);
         }
 
-        public override void CleanUp()
-        {
-            foreach (string _texture in textures)
-            {
-                File.Delete(InfoManager.currentDir + @$"\Temp\{_texture}.png");
-            }
-        }
-
         public override void Render()
         {
-            Vector3 camRight = InfoManager.engineCamera.Right;
-            Vector3 camUp = InfoManager.engineCamera.Up;
-            Vector2 remappedAnchor = Vector2.Zero;
-            remappedAnchor = new Vector2(anchor.X * 0.933f, -anchor.Y * 0.522f * (InfoManager.windowSize.X / InfoManager.windowSize.Y));
-            float hh = MathF.Tan(MathHelper.DegreesToRadians(InfoManager.engineCamera.Fov) / 2.0f) * 1;
-            float aspect = InfoManager.windowSize.X / InfoManager.windowSize.Y;
-            renderObject.position = InfoManager.engineCamera.Position + InfoManager.engineCamera.Front * 1
-                + remappedAnchor.X * camRight * hh * aspect
-                + remappedAnchor.Y * camUp * hh
-                + camRight * addedPosition.X * 0.007f
-                + camUp * addedPosition.Y * 0.007f;
             renderObject.scale = new Vector3(0.07f * (float)texture.Width, 0.07f * (float)texture.Height, 1f) * 0.025f * scale;
-            renderObject.SetRotation(InfoManager.engineCamera.GetViewMatrix().ExtractRotation().Inverted());
-            renderObject.Render();
+            base.Render();
         }
     }
 }
