c73a187c0b584d8327c81894fb7cf9d1459a8ffa
eyeseeyougood <97800461+eyeseeyougood@users.noreply.github.com>
2024-02-04 10:51:33 +0000

    Blinn-Phong Lighting
    + added Blinn-Phong Lighting

--- Steel Engine/Resources/Scripts/MoveTest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Steel_Engine;
-
-public class MoveTest : Component
-{
-    public override void Tick(float deltaTime)
-    {
-        gameObject.position += new OpenTK.Mathematics.Vector3(deltaTime, 0, 0);
-    }
-}
